0ddcf514b820a56b4b5a225993c0f38154f28542
abderrahimbkd <80970245+abderrahimbkd@users.noreply.github.com>
2025-09-30 19:27:52 +0100

    CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -77,15 +77,9 @@ export default function Intro() {
           Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        {/* <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
-          download>
-          English CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a> */}
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/public/CV.pdf"
+          href="/cv.pdf"
           download>
           French Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
